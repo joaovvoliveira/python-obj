8fe2597f39bfa5ee3f639120a9de2cb285308d2f
João Victor <jvictor_oliveira@Outlook.com.br>
2024-06-03 16:47:07 +0100

    Arrumando o código

--- bootcamp-DIO/first.py
+++ bootcamp-DIO/first.py
@@ -164,7 +164,6 @@ restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
                 {'nome':'Pizza Suprema', 'categoria':'Pizza', 'ativo':True},
                 {'nome':'Cantina', 'categoria':'Italiano', 'ativo':False}]
 
-
 def exibir_nome_do_programa():
     print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -178,12 +177,11 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
-    print('3. Ativar restaurante')
+    print('3. Alternar estado do restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
-    # os.system('clear') 
-    print('Finalizando o app\n')
+    exibir_subtitulo('Finalizar app')
 
 def voltar_ao_menu_principal():
     input('\nDigite uma tecla para voltar ao menu ')
@@ -208,7 +206,7 @@ def cadastrar_novo_restaurante():
     dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
     restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-
+    
     voltar_ao_menu_principal()
 
 def listar_restaurantes():
@@ -221,7 +219,7 @@ def listar_restaurantes():
         ativo = 'ativado' if restaurante['ativo'] else 'desativado'
         print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
 
-        voltar_ao_menu_principal()
+    voltar_ao_menu_principal()
 
 def alternar_estado_restaurante():
     exibir_subtitulo('ALterando estado do restaurante')
@@ -239,21 +237,21 @@ def alternar_estado_restaurante():
         print('O restaurante não foi encontrado')
             
     voltar_ao_menu_principal()
-    
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
+        # opcao_escolhida = int(opcao_escolhida)
 
-        if opcao_escolhida == 1:
+        if opcao_escolhida == 1: 
             cadastrar_novo_restaurante()
-        elif opcao_escolhida == 2:
-            listar_restaurante()
-        elif opcao_escolhida == 3:
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
             alternar_estado_restaurante()
-        elif opcao_escolhida == 4:
+        elif opcao_escolhida == 4: 
             finalizar_app()
-        else:
+        else: 
             opcao_invalida()
     except:
         opcao_invalida()
@@ -266,5 +264,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
